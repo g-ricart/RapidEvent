9fed2f3a9a6a04cc467ee721a2e46c7a4f8bd68d
gabouric <gabouric@gmail.com>
2022-05-03 15:49:53 +0200

    Implemented default constructor and destructor of RapidTrack

---
+++ src/RapidTrack.h
@@ -0,0 +1,15 @@
+#ifndef RAPIDEVENT_RAPIDTRACK_H_
+#define RAPIDEVENT_RAPIDTRACK_H_
+
+class RapidTrack {
+
+    public:
+        //! Default constructor.
+        RapidTrack();
+        //! Destructor.
+        ~RapidTrack();
+
+
+};
+
+#endif //RAPIDEVENT_RAPIDTRACK_H_
