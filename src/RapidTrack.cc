d52370bb1852dc8ff2364cc3a2816f767bedcd53
gabouric <gabouric@gmail.com>
2022-05-03 16:09:14 +0200

    Implemented standard constructor

--- src/RapidTrack.cc
+++ src/RapidTrack.cc
@@ -1,15 +1,23 @@
 #include "RapidTrack.h"
 
+#include "RapidConfig.h"
+
 using namespace std;
 
 //______________________________________________________________________________
 RapidTrack::RapidTrack()
 {
+    config_ = nullptr;
+}
 
+//______________________________________________________________________________
+RapidTrack::RapidTrack(RapidConfig* config)
+{
+    config_ = config;
 }
 
 //______________________________________________________________________________
 RapidTrack::~RapidTrack()
 {
-    
+    delete config_;
 }
--- src/RapidTrack.h
+++ src/RapidTrack.h
@@ -1,15 +1,20 @@
 #ifndef RAPIDEVENT_RAPIDTRACK_H_
 #define RAPIDEVENT_RAPIDTRACK_H_
 
+#include "RapidConfig.h"
+
 class RapidTrack {
 
     public:
         //! Default constructor.
         RapidTrack();
+        //! Standard constructor.
+        RapidTrack(RapidConfig* config);
         //! Destructor.
         ~RapidTrack();
 
-
+    private:
+        RapidConfig* config_;
 };
 
 #endif //RAPIDEVENT_RAPIDTRACK_H_
