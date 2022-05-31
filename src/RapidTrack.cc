b081780bfb91aab63b4e196366d2df95ef13c981
gabouric <gabouric@gmail.com>
2022-05-31 13:16:51 +0200

    comment

--- src/RapidTrack.cc
+++ src/RapidTrack.cc
@@ -77,7 +77,7 @@ void RapidTrack::SetPID()
 
     if (name_.Contains("pi")) {
         params_map_[TString("ProbNNpi")] = 1.;
-    } else if (name_.Contains("p")) {
+    } else if (name_.Contains("p")) { // check pi then p to avoid mis-ID
         params_map_[TString("ProbNNp")] = 1.;
     } else if (name_.Contains("K")) {
         params_map_[TString("ProbNNK")] = 1.;
