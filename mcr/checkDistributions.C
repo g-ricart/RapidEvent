787df97a7f0232903b0dd4881489736324aa854a
gabouric <gabouric@gmail.com>
2022-05-31 13:16:15 +0200

    Added macro to compare input/output distributions

---
+++ mcr/checkDistributions.C
@@ -0,0 +1,73 @@
+#include <iostream>
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1D.h"
+#include "TCanvas.h"
+#include "TRatioPlot.h"
+
+void checkDistributions() {
+
+    // histogram for rapidsim pions.
+    TH1D* pi_pt_h = new TH1D("pi_pt_h", "RapidSim pions pT", 100, 0, 2);
+
+    // histogram for rapidevent pions.
+    TH1D* pi_pt_event_h = new TH1D("pi_pt_event_h", "RapidEvent pions pT", 100, 0, 2);
+
+//_______________________________ pions ________________________________________
+    TFile* pip_file = TFile::Open("../data/pip_tree.root");
+    TTree* pip_tree = (TTree*)pip_file->Get("DecayTree");
+    pip_tree->SetBranchStatus("*", 0);
+    pip_tree->SetBranchStatus("pip_0_PT", 1);
+
+    Double_t pip_pt = 0;
+    pip_tree->SetBranchAddress("pip_0_PT", &pip_pt);
+    for (size_t i = 0; i < pip_tree->GetEntries(); i++) {
+        pip_tree->GetEntry(i);
+        pi_pt_h->Fill(pip_pt);
+    }
+
+    pip_file->Close();
+
+    TFile* pim_file = TFile::Open("../data/pim_tree.root");
+    TTree* pim_tree = (TTree*)pim_file->Get("DecayTree");
+    pim_tree->SetBranchStatus("*", 0);
+    pim_tree->SetBranchStatus("pim_0_PT", 1);
+
+    Double_t pim_pt = 0;
+    pim_tree->SetBranchAddress("pim_0_PT", &pim_pt);
+    for (size_t i = 0; i < pim_tree->GetEntries(); i++) {
+        pim_tree->GetEntry(i);
+        pi_pt_h->Fill(pim_pt);
+    }
+
+    pim_file->Close();
+
+    TFile* track_file = TFile::Open("../output/incorrectPrompts.root");
+    TTree* track_tree = (TTree*)track_file->Get("trackTree");
+    track_tree->SetBranchStatus("*", 0);
+    track_tree->SetBranchStatus("PT", 1);
+    track_tree->SetBranchStatus("ProbNNpi", 1);
+
+    Double_t pt = 0;
+    Double_t ProbNNpi = 0;
+    track_tree->SetBranchAddress("PT", &pt);
+    track_tree->SetBranchAddress("ProbNNpi", &ProbNNpi);
+
+    for (size_t i = 0; i < track_tree->GetEntries(); i++) {
+        track_tree->GetEntry(i);
+        if (ProbNNpi > 0.8) {
+            pi_pt_event_h->Fill(pt);
+        }
+    }
+
+    track_file->Close();
+
+//_______________________________plot___________________________________________
+
+    // normalise histograms
+    pi_pt_event_h->Scale(pi_pt_h->GetEntries()/pi_pt_event_h->GetEntries());
+    TCanvas* c1 = new TCanvas("c1", "", 0, 0, 1000, 800);
+    TRatioPlot* pi_plot = new TRatioPlot(pi_pt_event_h, pi_pt_h);
+    pi_plot->Draw();
+}
