ead1bd69fa8e7400134960af53de0864e8799111
gabouric <gabouric@gmail.com>
2022-06-22 12:32:29 +0200

    Added PX PY PZ as parameters

--- mcr/Analysis.C
+++ mcr/Analysis.C
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void backgroundAna(TString input_file_name) {
+void singleMuonBkgAna(TString input_file_name) {
 
     TFile* input_file = new TFile(input_file_name, "READ");
     TTree* track_tree = (TTree*)input_file->Get("trackTree");
@@ -88,4 +88,57 @@ void backgroundAna(TString input_file_name) {
     input_file->Close();
     delete input_file;
     delete part_name;
+}
+
+void muonPairBkgAna(TString file_name) {
+
+    TFile* input_file = new TFile(input_file_name, "READ");
+    TTree* track_tree = (TTree*)input_file->Get("trackTree");
+
+    // Build TTree index
+    track_tree->BuildIndex("eventNumber", "trackID");
+
+    // Set branch addresses
+    Ssiz_t event_number;
+    Size_t track_id, mother_id;
+    TString* part_name = new TString();
+    Bool_t is_prompt;
+    Double_t fd,    fd_true;
+    Double_t ip,    ip_true;
+    Double_t p,     p_true;
+    Double_t pt,    pt_true;
+    Double_t px,    px_true;
+    Double_t py,    py_true;
+    Double_t pz,    pz_true;
+    Double_t eta,   eta_true;
+    Double_t origX, origx_true;
+    Double_t origY, origY_true;
+    Double_t origZ, origZ_true;
+    Double_t probNNk, probNNmu, probNNpi, probNNp;
+    track_tree->SetBranchAddress("eventNumber", &event_number);
+    track_tree->SetBranchAddress("trackID",     &track_id);
+    track_tree->SetBranchAddress("motherID",    &mother_id);
+    track_tree->SetBranchAddress("partName",    &part_name);
+    track_tree->SetBranchAddress("isPrompt",    &is_prompt);
+    track_tree->SetBranchAddress("ProbNNmu",    &probNNmu);
+    track_tree->SetBranchAddress("PX",          &px);
+    track_tree->SetBranchAddress("PY",          &py);
+    track_tree->SetBranchAddress("PZ",          &pz);
+    track_tree->SetBranchAddress("PX_TRUE",     &px_true);
+    track_tree->SetBranchAddress("PY_TRUE",     &py_true);
+    track_tree->SetBranchAddress("PZ_TRUE",     &pz_true);
+
+    track_tree->SetBranchStatus("*", 0);
+    track_tree->SetBranchStatus("eventNumber", 1);
+    track_tree->SetBranchStatus("trackID",     1);
+    track_tree->SetBranchStatus("motherID",    1);
+    track_tree->SetBranchStatus("partName",    1);
+    track_tree->SetBranchStatus("isPrompt",    1);
+    track_tree->SetBranchStatus("ProbNNmu",    1);
+    track_tree->SetBranchStatus("PX",          1);
+    track_tree->SetBranchStatus("PY",          1);
+    track_tree->SetBranchStatus("PZ",          1);
+    track_tree->SetBranchStatus("PX_TRUE",     1);
+    track_tree->SetBranchStatus("PY_TRUE",     1);
+    track_tree->SetBranchStatus("PZ_TRUE",     1);
 }
