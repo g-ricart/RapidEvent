b2bc2774487fb9eea11f5bc251dca0cd9453085f
g-ricart <gabriel.ricart@outlook.fr>
2022-06-23 02:37:14 +0200

    Added ID histogram

--- mcr/Analysis.C
+++ mcr/Analysis.C
@@ -3,9 +3,35 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TH1D.h"
+#include "TH1I.h"
 #include "TString.h"
+#include "TCanvas.h"
 
 using namespace std;
+
+void checkIDs(TString input_file_name) {
+
+    TFile* input_file = new TFile(input_file_name, "READ");
+    TTree* track_tree = (TTree*)input_file->Get("trackTree");
+
+    TH1I* id_hist = new TH1I("id_hist", "", 600, 0, 600);
+
+    Size_t track_id;
+    track_tree->SetBranchAddress("trackID", &track_id);
+
+    track_tree->SetBranchStatus("*", 0);
+    track_tree->SetBranchStatus("trackID", 1);
+
+    size_t n_entries = track_tree->GetEntries();
+
+    for (size_t entry_id = 0; entry_id < n_entries; entry_id++) {
+        track_tree->GetEntry(entry_id);
+        id_hist->Fill(track_id);
+    }
+
+    TCanvas* c1 = new TCanvas("c1", "", 1000, 800);
+    id_hist->Draw();
+}
 
 void singleMuonBkgAna(TString input_file_name) {
 
@@ -48,37 +74,51 @@ void singleMuonBkgAna(TString input_file_name) {
     Ssiz_t n_signal     = 0;
     Ssiz_t n_background = 0;
 
+    Ssiz_t n_gamma  = 0;
+    Ssiz_t n_D0_D0b = 0;
+
     size_t n_entries = track_tree->GetEntries();
 
     for (size_t entry_id = 0; entry_id < n_entries; entry_id++) {
 
-        if (entry_id % 10000 == 0) {
-            cout << entry_id << "/" << n_entries << "\r";
-        }
-
         track_tree->GetEntry(entry_id);
 
-        if (probNNmu < 0.8) { // Skip if not a muon.
-            continue;
-        }
-
-        n_muons++;
-
-        // If track is a muon, get mother track name.
-        track_tree->GetEntryWithIndex(event_number, mother_id);
-
         if (part_name->Contains("gamma")) {
-            n_signal++;
+            n_gamma++;
             continue;
         }
 
         if (part_name->Contains("D0")) {
-            n_background++;
+            n_D0_D0b++;
+            continue;
+        }
+
+        if (probNNmu > 0.8) {
+
+            n_muons++;
+
+            // If track is a muon, get mother track name.
+            track_tree->GetEntryWithIndex(event_number, mother_id);
+
+            if (part_name->Contains("gamma")) {
+                n_signal++;
+                continue;
+            }
+
+            if (part_name->Contains("D0")) {
+                n_background++;
+                continue;
+            }
+
+            cout << "wtf" << endl;
             continue;
         }
     }
 
     cout << endl;
+    cout << "----------------------------------------"             << endl;
+    cout << "gamma : "      << n_gamma                             << endl;
+    cout << "D0 + D0b : "   << n_D0_D0b                            << endl;
     cout << "------------single muon data------------"             << endl;
     cout << "total : "      << n_muons                             << endl;
     cout << "signal : "     << n_signal                            << endl;
@@ -90,7 +130,7 @@ void singleMuonBkgAna(TString input_file_name) {
     delete part_name;
 }
 
-void muonPairBkgAna(TString file_name) {
+void muonPairBkgAna(TString input_file_name) {
 
     TFile* input_file = new TFile(input_file_name, "READ");
     TTree* track_tree = (TTree*)input_file->Get("trackTree");
